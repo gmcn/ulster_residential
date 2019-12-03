dfc2160df379e1b47c8cc1aadefad2e4f3d5dc89
Web Dev <webdev@Webs-MBP.lan>
2019-12-03 10:05:45 +0000

    updating json stockists query

--- wp-content/themes/starting-theme/archive-stockists.php
+++ wp-content/themes/starting-theme/archive-stockists.php
@@ -1,60 +1,51 @@
 <?php
 
-header("Content-Type: application/json; charset=UTF-8");
+// WP_Query arguments
+$args = array (
+
+'post_type'       => 'stockists',
+'post_status'     => 'publish',
+'posts_per_page'  => -1,
+'no_found_rows=true' => true,
+
+);
+
+// The Query
+$loop = new WP_Query( $args );
+
+// create an array if there is more than one result
+$locations = array();
+
+//the loop
+while ( $loop->have_posts() ) : $loop->the_post();
+
+   // Add in your custom fields or WP fields that you want
+   $locations[] = array(
+     'id' => get_the_ID(),
+     'name' => get_the_title(),
+     'address1' => get_field('address1'),
+     'address2' => get_field('address2'),
+     'address3' => get_field('address3'),
+     'city' => get_field('city'),
+     'zip' => get_field('zip'),
+     'phone' => get_field('phone'),
+     'email' => get_field('email'),
+     'web' => get_field('web'),
+     'stockist_id' => get_field('stockist_id'),
+     'lat' => get_field('lat'),
+     'lng' => get_field('lng')
+   );
+
+endwhile;
+
+wp_reset_query();
+
+// output
+
+echo json_encode( $locations, JSON_PRETTY_PRINT );
 
 
-//get_header();
+
 ?>
-
-  <?php
-
-  // WP_Query arguments
-  $args = array (
-
-  'post_type'       => 'stockists',
-  'post_status'     => 'publish',
-  'posts_per_page'  => -1,
-
-  );
-
-  // The Query
-  $loop = new WP_Query( $args );
-
-  //the loop
-  while ( $loop->have_posts() ) : $loop->the_post();
-
-    // create an array if there is more than one result
-    // $locations = array();
-
-    $title = get_the_title();
-
-     // Add in your custom fields or WP fields that you want
-     $locations[] = array(
-       'id' => get_the_ID(),
-       'name' => $title,
-       'address1' => get_field('address1'),
-       'address2' => get_field('address2'),
-       'address3' => get_field('address3'),
-       'city' => get_field('city'),
-       'zip' => get_field('zip'),
-       'phone' => get_field('phone'),
-       'email' => get_field('email'),
-       'web' => get_field('web'),
-       'stockist_id' => get_field('stockist_id'),
-       'lng' => get_field('lng'),
-       'lat' => get_field('lat')
-     );
-
-  endwhile;
-
-  wp_reset_query();
-
-  // output
-
-  echo json_encode( $locations, JSON_PRETTY_PRINT );
-
-
-
-  ?>
-  <?php
-  //get_footer();
+<?php
+//get_footer();
