f531beb97e74c5147904388e96ea3a1fa1980d90
Web Dev <webdev@Webs-MBP.lan>
2019-12-03 10:06:45 +0000

    improved responsiveness

--- wp-content/themes/starting-theme/inc/page-rugs/content.php
+++ wp-content/themes/starting-theme/inc/page-rugs/content.php
@@ -34,7 +34,7 @@
 
     ?>
 
-    <div class="row step step<?php echo $i ?>" style="background: url(<?php echo $section_image; ?>) fixed; margin-bottom: 15px; background-size: cover">
+    <div class="row step step<?php echo $i ?>" style="background: url(<?php echo $section_image; ?>) fixed; margin-bottom: 15px;">
 
       <style media="screen">
         .step<?php echo $i ?> .st0 {
